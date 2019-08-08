5ee27b4e7f8b5b39ee3c492e8eca1442935dcc94
srikanthpragada <srikanthpragada@gmail.com>
2019-08-08 20:57:15 +0530

    08-AUG

--- training/models.py
+++ training/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Student(models.Model):
+    fullname = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50, null=True)
+    course = models.CharField(max_length=5)
+    feepaid = models.IntegerField()
+
+    def __str__(self):
+        return self.fullname + "," + self.course
